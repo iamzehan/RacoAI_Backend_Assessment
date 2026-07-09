235a1eddf88ff9226b567a8839caff32f5ca4c1d
Zehan <zehan.kareem@gmail.com>
2026-07-09 19:38:31 +0600

    feat(types): Added TokenCredetials type

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -2,3 +2,13 @@ interface UserInput {
     email: string;
     password: string;
 }
+
+// Token Credentials sent as cookie
+interface TokenCredentials{
+    user: {
+        userId: string;
+        email: string;
+    },
+    accessToken: string;
+    refreshToken: string;
+}
