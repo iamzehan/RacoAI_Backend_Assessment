72466e2aa95e8839c7b64c3cc467d93c1e994ba3
Zehan <zehan.kareem@gmail.com>
2026-07-09 23:10:05 +0600

    fix(iamzehan): Updated auth routes to use service, repository, controller conventions

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,35 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { AuthController } from "../controllers/auth.controller.js";
+import { AuthService } from "../services/auth.service.js";
+import { UserRepository } from "../repositories/user.repository.js";
+import { PasswordService } from "../services/password.service.js";
+import { TokenService } from "../services/token.service.js";
+import { UserService } from "../services/user.service.js";
+import { RefreshController } from "../controllers/refresh.controller.js";
+import { AuthenticationMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+// Dependencies
+const userRepository = new UserRepository();
+const passwordService = new PasswordService();
+const tokenService = new TokenService();
+
+// Authentication Service
+const authService = new AuthService(
+  userRepository,
+  passwordService,
+  tokenService
+);
+
+// Controllers
+const authController = new AuthController(authService);
+const refreshController = new RefreshController(authService);
+
+// routes
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
+router.post("/refresh", refreshController.refresh);
 
 export default router;
