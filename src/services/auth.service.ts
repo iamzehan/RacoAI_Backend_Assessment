90a5172ff7fcb3129faec8524d207834e7265078
Zehan <zehan.kareem@gmail.com>
2026-07-09 18:59:21 +0600

    service(init): Add auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,73 @@
+import { refresh } from "../controllers/refresh.controller.js";
+import { User } from "../generated/prisma/client.js";
+import { UserRepository } from "../repositories/user.repository.js";
+import { PasswordService } from "./password.service.js";
+import { TokenService } from "./token.service.js";
+
+export class UserService {
+  constructor(
+    private userRepository: UserRepository,
+    private passwordService: PasswordService,
+    private tokenService: TokenService
+  ) {}
+
+  //   REGISTER USER
+  async register(data: UserInput) {
+    // unpack credentials
+    const { email, password } = data;
+    // Check if email exists
+    const existing = await this.userRepository.findByEmail(email);
+    if (existing) {
+      throw new Error("Email already exists");
+    }
+    // If user doesn't exist then create user
+
+    // hash password
+    const passwordHash = await this.passwordService.hashPassword(password);
+    // create new user
+    return this.userRepository.createUser({ email, password: passwordHash });
+  }
+
+  //   UPDATE USER DATA
+  async update(data: UserInput) {
+    // unpack credentials
+    const { email, password } = data;
+    // Check if email exists
+    const existing = await this.userRepository.findByEmail(email);
+    if (existing) {
+      // hash password
+      const passwordHash = await this.passwordService.hashPassword(password);
+      // create new user
+      return this.userRepository.createUser({ email, password: passwordHash });
+    } else {
+      throw new Error("Email is not registered");
+    }
+  }
+
+  //   LOGIN USER
+  async login(data: UserInput) {
+    // unpack credentials
+    const { email, password } = data;
+    // get registered user
+    const user = await this.userRepository.findByEmail(email);
+
+    // compare passwords
+    if (
+      !user ||
+      !(await this.passwordService.comparePassword(password, user.password))
+    ) {
+      throw new Error("Invalid Credentials!");
+    }
+    // get JWT Tokens
+    const accessToken = this.tokenService.signAccessToken(user.id)
+    const refreshToken = this.tokenService.signRefreshToken(user.id)
+    return {
+        user: {
+            userId: user.id,
+            email
+        },
+        accessToken,
+        refreshToken
+    }
+  }
+}
