007ee98ed6c20f6db1a11abd25a9760bc4bdd6ed
Zehan <zehan.kareem@gmail.com>
2026-07-09 23:10:30 +0600

    feat(router): Added v1 api route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+
+// auth routes
 import authRoutes from "./auth.routes.js";
+
+// version 1 api routes
+import v1 from "./v1/index.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/v1", v1)
 
 export default router;
