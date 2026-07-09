425f6ab77446c5b4465e6063ef2f30f68188872b
Zehan <zehan.kareem@gmail.com>
2026-07-09 19:38:07 +0600

    feat(service): Added verify JWT method

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,11 +1,28 @@
 import jwt from "jsonwebtoken";
 import { env } from "../config/env.js";
 
+type Token = "access" | "refresh";
 export class TokenService {
   constructor() {}
+  // sign access token
   signAccessToken = (userId: string) =>
     jwt.sign({ sub: userId }, env.JWT_SECRET, { expiresIn: "15m" });
 
+  // Sign refresh token
   signRefreshToken = (userId: string) =>
     jwt.sign({ sub: userId }, env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+
+  // verify token
+  verifyJWT = (token: string, category: Token) => {
+    const secret =
+      category === "refresh" ? env.JWT_REFRESH_SECRET : env.JWT_SECRET;
+    try {
+      const payload = jwt.verify(token, secret) as {
+        sub: string;
+      };
+      return payload;
+    } catch {
+      return false;
+    }
+  };
 }
