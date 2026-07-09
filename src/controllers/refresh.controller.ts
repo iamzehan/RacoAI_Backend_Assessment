eb73570d97257f3d3b4b5d99fba9cc25e12b04ab
Zehan <zehan.kareem@gmail.com>
2026-07-09 20:25:05 +0600

    feat(controller): Refactored refresh controller with auth services.

--- src/controllers/refresh.controller.ts
+++ src/controllers/refresh.controller.ts
@@ -1,20 +1,31 @@
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import { env } from "../config/env.js";
-import { signAccessToken } from "../utils/jwt.js";
+import { UserService } from "../services/auth.service.js";
 
-export const refresh = (req: Request, res: Response) => {
-  const token = req.session.refreshToken;
-  if (!token) return res.sendStatus(401);
+class RefreshController {
+  constructor(private userService: UserService) {}
 
-  try {
-    const payload = jwt.verify(token, env.JWT_REFRESH_SECRET) as {
-      sub: string;
-    };
-
-    const accessToken = signAccessToken(payload.sub);
-    res.json({ accessToken });
-  } catch {
-    res.sendStatus(401);
-  }
-};
+  refresh = async (req: Request, res: Response) => {
+    const token = req.session.refreshToken;
+    if (!token) return res.sendStatus(401);
+    try {
+      const credentials = await this.userService.refresh(token);
+      // http cookie response with credentials
+      res
+        .cookie("accessToken", credentials.accessToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "lax",
+          maxAge: 15 * 60 * 1000
+        })
+        .cookie("refreshToken", credentials.refreshToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "strict",
+          maxAge: 30 * 24 * 60 * 60 * 1000
+        })
+        .json(credentials.user);
+    } catch (err) {
+      res.status(401).json({ message: err });
+    }
+  };
+}
