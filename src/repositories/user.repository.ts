c4b85c3578133b16ba9c0a7f889b545bf179612a
Zehan <zehan.kareem@gmail.com>
2026-07-09 18:58:51 +0600

    db(add): Add user Repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,33 @@
+// import prisma from config
+import { prisma } from "../config/prisma.js";
+import { User } from "../generated/prisma/client.js";
+// user repo
+export class UserRepository {
+  constructor() {}
+  // find user by email if exists
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: { email: email }
+    });
+    return user;
+  }
+  // create user
+  async createUser(data: UserInput) {
+    return prisma.user.create({
+      data
+    });
+  }
+  // update user
+  async updateUser(data: UserInput) {
+    const { email, password } = data;
+    return prisma.user.update({
+      where: {
+        email
+      },
+      data: {
+        email,
+        password
+      }
+    });
+  }
+}
