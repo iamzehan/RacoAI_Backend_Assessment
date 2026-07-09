6670cbfac2be681a28a5cff3c058274429fa3ee3
Zehan <zehan.kareem@gmail.com>
2026-07-09 23:09:15 +0600

    refactor(middleware): Refactored middleware functions to classbased methods

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,38 +1,40 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { env } from "../config/env.js";
+import { TokenService } from "../services/token.service.js";
 
-export const requireAuth = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.sendStatus(401);
+export class AuthenticationMiddleware {
+  constructor(private tokenService: TokenService) {}
 
-  try {
-    const payload = jwt.verify(token, env.JWT_SECRET) as { sub: string };
-    req.userId = payload.sub;
-    next();
-  } catch {
-    res.sendStatus(401);
-  }
-};
+  // Route Requires authentication
+  requireAuth = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.sendStatus(401);
 
-export const ensureGuest = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) return next(); // No token → guest allowed
-
-  const token = authHeader.split(" ")[1];
-  if (!token) return next();
-
-  try {
-    const payload = jwt.verify(token, env.JWT_SECRET) as { sub: string };
-    if (payload.sub) {
-      return res.status(403).json({ message: "Already authenticated" });
+    try {
+      const payload = this.tokenService.verifyJWT(token, "access");
+      req.userId = payload.sub;
+      next();
+    } catch {
+      res.sendStatus(401);
     }
-    next();
-  } catch {
-    next(); // Invalid token → treat as guest
-  }
-};
+  };
+  // Ensure user is a guest
+  ensureGuest = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return next(); // No token → guest allowed
+
+    const token = authHeader.split(" ")[1];
+    if (!token) return next();
+
+    try {
+      const payload = this.tokenService.verifyJWT(token, "access");
+      if (payload.sub) {
+        return res.status(403).json({ message: "Already authenticated" });
+      }
+      next();
+    } catch {
+      next(); // Invalid token → treat as guest
+    }
+  };
+}
