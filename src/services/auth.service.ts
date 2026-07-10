9b6456b858bdd7af9aabdd552c4f2818a8cd35af
Zehan <zehan.kareem@gmail.com>
2026-07-10 16:45:22 +0600

    fix(service): Import path changed

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { User as type, User} from "../generated/prisma/client.js";
 import { UserRepository } from "../repositories/user.repository.js";
 import { TokenCredentials } from "../types/credentials.js";
+import { UserInfo } from "../types/user.js";
 import { PasswordService } from "./password.service.js";
 import { TokenService } from "./token.service.js";
 
