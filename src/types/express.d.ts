db718e8a67508dccc56944a040aa4fb533a3eef0
Zehan <zehan.kareem@gmail.com>
2026-07-10 16:47:14 +0600

    feat(type): Added role

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,8 +1,10 @@
 import "express";
+import { Role } from "../generated/prisma/enums.ts";
 declare global {
   namespace Express {
     interface Request {
       userId?: string;
+      userRole?: Role
     }
   }
 }
