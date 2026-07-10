a7c8ac4c36c5e8109f74da316856a77c3d97ebd3
Zehan <zehan.kareem@gmail.com>
2026-07-10 16:36:09 +0600

    refactor(controller): Using services collection as import

--- src/controllers/_index.ts
+++ src/controllers/_index.ts
@@ -1,36 +1,19 @@
-// import repositories
-import { userRepository } from "../repositories/_index.js";
-import { productRepository } from "../repositories/_index.js";
 
 // import services
-import { UserService } from "../services/user.service.js";
-import { TokenService } from "../services/token.service.js";
-import { PasswordService } from "../services/password.service.js";
-import { AuthService } from "../services/auth.service.js";
+import services from "../services/_index.js";
 
 // import controllers
 import { AuthController } from "./auth.controller.js";
 import { UserController } from "./user.controller.js";
 import { RefreshController } from "./refresh.controller.js";
 import { ProductController } from "./product.controller.js";
-import { ProductService } from "../services/product.service.js";
 
-// Dependencies
-const userService = new UserService(userRepository);
-const tokenService = new TokenService();
-const passwordService = new PasswordService();
-const authService = new AuthService(
-  userRepository,
-  passwordService,
-  tokenService
-);
-const productService = new ProductService(productRepository);
 
 // Controllers
-const authController = new AuthController(authService);
-const refreshController = new RefreshController(authService);
-const userController = new UserController(userService);
-const productController = new ProductController(productService);
+const authController = new AuthController(services.authService);
+const refreshController = new RefreshController(services.authService);
+const userController = new UserController(services.userService);
+const productController = new ProductController(services.productService);
 
 // Add to controller collection
 const controller = {
