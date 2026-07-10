009f32c8c18e2ebcf662e736c0b7e576826e248d
Zehan <zehan.kareem@gmail.com>
2026-07-10 17:06:47 +0600

    db(migrations): new prisma migration

---
+++ src/prisma/migrations/20260710110624_init/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE "CategoriesOnProducts" DROP CONSTRAINT "CategoriesOnProducts_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CategoriesOnProducts" DROP CONSTRAINT "CategoriesOnProducts_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Stock" DROP CONSTRAINT "Stock_productId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "CategoriesOnProducts" ADD CONSTRAINT "CategoriesOnProducts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CategoriesOnProducts" ADD CONSTRAINT "CategoriesOnProducts_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Categories"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Stock" ADD CONSTRAINT "Stock_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
