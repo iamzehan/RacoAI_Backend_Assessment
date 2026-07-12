da6eb2a5c3c71b1eaa0b35eb34e5ed0ae71f1518
Zehan <zehan.kareem@gmail.com>
2026-07-12 23:09:23 +0600

    feat(services): Updated redis service

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,26 +1,37 @@
 import { createClient } from "redis";
-import { env } from "../config/env.js";
 
 class RedisService {
-    // create client
   private client = createClient({
-    url: env.REDIS_URL,
+    url: process.env.REDIS_URL,
   });
 
-//   set connection
   async connect() {
-    this.client.on("error", (err) => {
-      console.error("Redis Error:", err);
-    });
-
     await this.client.connect();
-
-    console.log("Redis Connected");
   }
 
-//   get the client
-  getClient() {
-    return this.client;
+  async set(key: string, value: unknown, ttl?: number) {
+    const serialized = JSON.stringify(value);
+
+    if (ttl) {
+      await this.client.set(key, serialized, { EX: ttl });
+      return;
+    }
+
+    await this.client.set(key, serialized);
+  }
+
+  async get<T>(key: string): Promise<T | null> {
+    const value = await this.client.get(key);
+
+    return value ? (JSON.parse(value) as T) : null;
+  }
+
+  async delete(key: string) {
+    await this.client.del(key);
+  }
+
+  async exists(key: string) {
+    return (await this.client.exists(key)) === 1;
   }
 }
 
