00895f2ff169235f2ecc2a520e9c43fcf6ec6fa6
Zehan <zehan.kareem@gmail.com>
2026-07-13 20:30:50 +0600

    utils(cache): Added order(s) cache key

--- src/utils/cache-key.ts
+++ src/utils/cache-key.ts
@@ -1,11 +1,26 @@
+import { OrderQuery } from "../types/order.js";
 import { ProductQuery } from "../types/product.js";
 
 export class CacheKey {
+
+  // Cache key for product(s)
+
   static product(id: string) {
     return `product:${id}`;
   }
-
+  
   static products(query: ProductQuery) {
     return `products:${JSON.stringify(query)}`;
+  }
+
+  // Cache key for order(s)
+  static order(id: string) {
+    return `order:${id}`;
+  }
+  static orderNum(orderNumber: string){
+    return `orderNum: ${orderNumber}`
+  }
+  static orders(query: OrderQuery) {
+    return `orders: ${JSON.stringify(query)}`;
   }
 }
