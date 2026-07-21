00e54079920cd38e13a892377bf32b0c14b3c116
Zehan <zehan.kareem@gmail.com>
2026-07-22 01:38:39 +0600

    feat(validation): Created a validation middleware for for auth routes and used them in the routes /register and /login

--- src/middlewares/_index.ts
+++ src/middlewares/_index.ts
@@ -2,12 +2,16 @@ import { UserRepository } from "../repositories/user.repository.js";
 import { UserService } from "../services/user.service.js";
 import { TokenService } from "../services/token.service.js";
 import { AuthenticationMiddleware } from "./auth.middleware.js";
+import { ValidationMiddleware } from "./validator.middleware.js";
 
 const userRepository = new UserRepository();
 const userService = new UserService(userRepository);
 const tokenService = new TokenService();
 
+
 export const auth = new AuthenticationMiddleware(
   tokenService,
-  userService
+  userService,
 );
+// Validation middleware
+export const validation = new ValidationMiddleware();
