5c09f9fa195b984d75d5f7ae5c7913a9a68f2080
Zehan <zehan.kareem@gmail.com>
2026-07-22 02:09:17 +0600

    feat(user): Added new methods to find existing user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,19 +1,45 @@
+import { User } from "../generated/prisma/client.js";
 import { UserRepository } from "../repositories/user.repository.js";
 import { Profile } from "../types/user.js";
 
 export class UserService {
   constructor(private userRepository: UserRepository) {}
-  // Profile
-  profile = async (userId: string): Promise<Profile> => {
-    const user = await this.userRepository.findById(userId);
-    if (!user) throw new Error("User not found!");
+
+  private filterResponse =(user: User): Profile => {
     return {
-      id: user?.id || userId,
+      id: user?.id,
       email: user?.email,
       username: user?.username,
       firstName: user?.firstName,
       lastName: user?.lastName,
       role: user?.role
     };
+  }
+  // Profile
+  profile = async (userId: string): Promise<Profile> => {
+    const user = await this.userRepository.findById(userId);
+    if (!user) throw new Error("User not found!");
+    return this.filterResponse(user);
   };
+
+  // find user by email 
+  findByEmail = async (email: string): Promise<Profile> => {
+    const user = await this.userRepository.findByEmail(email);
+    if(!user) throw new Error("User not found!");
+    return this.filterResponse(user);
+  }
+
+  // find user by username 
+  findByUserName = async (username: string) : Promise<Profile> => {
+    const user = await this.userRepository.findByUserName(username);
+    if(!user) throw new Error("User not found!");
+    return this.filterResponse(user);
+  }
+
+  // find existing user
+  findExistingUser = async (email: string, username: string)=> {
+    const user = await this.userRepository.findExistingUser(email, username);
+    if(!user) throw new Error("User not found!");
+    return this.filterResponse(user);
+  }
 }
